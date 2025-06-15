30f526a0c72625784b6ffb6527f031fb2204cfb4
Vlad <berezin.v.7930@gmail.com>
2025-06-15 14:37:19 +0300

    Replace BrowserRouter with HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header';
